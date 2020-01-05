59f20107f4a9a2a092b61fede217aca67666bb38
Chris <chris.conner57@gmail.com>
2020-01-04 21:44:51 -0800

    Fix formatting

--- Modern React Bootcamp/lights-out/src/App.js
+++ Modern React Bootcamp/lights-out/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import Board from "./Board";
-import "./App.css";
+import React, { Component } from 'react';
+import Board from './Board';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Board />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Board />
+			</div>
+		);
+	}
 }
 
 export default App;
