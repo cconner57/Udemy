76a6da0dd74412d8b18b555ccfdfbbfa83d4330c
Chris <chris.conner57@gmail.com>
2020-01-21 12:16:39 -0800

    Added Search

--- React Front to Back/github_finder/src/components/users/Search.js
+++ React Front to Back/github_finder/src/components/users/Search.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class Search extends Component {
 	state = {
 		text: ''
-	};
+    };
+    
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    }
 
 	onSubmit = (e) => {
 		e.preventDefault();
